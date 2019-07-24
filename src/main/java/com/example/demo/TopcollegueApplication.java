8513e8d1779e69b0349d5fa7ef0798997079c73c
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-24 14:25:56 +0200

    START Sp web-starter, devtools, h2, jpa

---
+++ src/main/java/com/example/demo/TopcollegueApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopcollegueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopcollegueApplication.class, args);
+	}
+
+}
